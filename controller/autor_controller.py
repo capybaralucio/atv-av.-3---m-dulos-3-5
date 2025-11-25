894cc3be7e492de512d979425724f4b56fd05eec
lucio <lucio.alvezz1829@gmail.com>
2025-11-24 23:37:16 -0300

    Git@GitHub.com capybaralucio/biblioteca universitaria atv3.git (#8)
    código finalizado

--- controller/autor_controller.py
+++ controller/autor_controller.py
@@ -11,20 +11,58 @@ class AutorController:
     def cadastrar_autor(self, nome, nacionalidade):
         if not nome:
             return "\nO nome do autor não pode ser nulo.\n"
+<<<<<<< HEAD
+
+        sucesso = self.model.cadastrar_autor(nome, nacionalidade)
+
+        if sucesso:    
+            return "\nAutor cadastrado com sucesso!!\n"
+        else:
+            return "\nErro ao cadastrar autor.\n"
+=======
         self.model.cadastrar_autor(nome, nacionalidade)
         return "\nAutor cadastrado com sucesso!!\n"
+>>>>>>> b4da2420e70473d557b3618dd00105dacdae441c
 
 
     def listar_autores(self):
         autores = self.model.listar_autores()
+<<<<<<< HEAD
+=======
 
         if not autores:
             return "\nNenhum autor encontrado.\n"
         
+>>>>>>> b4da2420e70473d557b3618dd00105dacdae441c
         return autores
     
 
     def atualizar_autor(self, id_autor, novo_nome=None, nova_nacionalidade=None):
+<<<<<<< HEAD
+        if not str(id_autor).isdigit():
+            return "\nID inválido.\n"
+
+        if novo_nome is None and nova_nacionalidade is None:
+            return "\nNada para atualizar.\n"
+
+        sucesso = self.model.atualizar_autor(id_autor, novo_nome, nova_nacionalidade)
+        
+        if sucesso:
+            return "\nAutor atualizado com sucesso!!\n"
+        else:
+            return "\nErro ao atualizar o autor.\n"
+
+    def excluir_autor(self, id_autor):
+        if not str(id_autor).isdigit():
+            return "\nID inválido.\n"
+        
+        sucesso = self.model.excluir_autor(id_autor)
+
+        if sucesso:
+            return "\nAutor excluído com sucesso!!\n"
+        else:
+            return f"\nAutor não encontrado ou erro ao excluir.\n"
+=======
         if not id_autor:
             return "\nID inválido.\n"
         
@@ -45,3 +83,4 @@ class AutorController:
         
         except Exception as e:
             return f"\nErro ao excluir o autor: {e}\n"
+>>>>>>> b4da2420e70473d557b3618dd00105dacdae441c
--- view/menu_view.py
+++ view/menu_view.py
@@ -4,8 +4,16 @@ from view.autor_view import AutorView
 from view.livro_view import LivroView
 
 class MenuView:
+<<<<<<< HEAD
+    def __init__(self):
+        self.autor_view = AutorView()
+        self.livro_view = LivroView()
+
+    def exibir_menu(self):
+=======
 
     def menu_principal(self):
+>>>>>>> b4da2420e70473d557b3618dd00105dacdae441c
         while True:
             print("\n===== MENU PRINCIPAL =====")
             print("1 - Gerenciar autor")
@@ -14,6 +22,15 @@ class MenuView:
             op = input("Escolha uma opção: ")
 
             if op == "1":
+<<<<<<< HEAD
+                self.autor_view.menu_autor()
+            
+            elif op == "2":
+                self.livro_view.menu_livro()
+
+            elif op == "3":
+                print("Encerrando o programa da Biblioteca. . .")
+=======
                 AutorView().menu()
             
             elif op == "2":
@@ -21,6 +38,7 @@ class MenuView:
 
             elif op == "3":
                 print("Encerrando o programa da Bilioteca. . .")
+>>>>>>> b4da2420e70473d557b3618dd00105dacdae441c
                 break
 
             else:
